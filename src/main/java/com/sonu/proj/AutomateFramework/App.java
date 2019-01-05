0df6b07550ef49e41b24be2f7027c47350f1b6fa
SonaliDighe <dighesonalishinde@gmail.com>
2019-01-05 09:48:38 -0800

    first commit

---
+++ src/main/java/com/sonu/proj/AutomateFramework/App.java
@@ -0,0 +1,13 @@
+package com.sonu.proj.AutomateFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
